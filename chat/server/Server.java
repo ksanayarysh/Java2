1d2f2e0e49817a5cc72d41e158dd7bfea61952a0
Ксения Ярыш <ksana32@gmail.com>
2019-03-30 15:06:58 +0500

    console chat

---
+++ chat/server/Server.java
@@ -0,0 +1,61 @@
+package chat.server;
+
+
+import chat.network.TCPConnection;
+import chat.network.TCPConnectionListener;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+
+public class Server implements TCPConnectionListener {
+    public static void main(String[] args) {
+        new Server();
+    }
+
+    private final ArrayList<TCPConnection> connections = new ArrayList<>();
+
+    private Server() {
+        System.out.println("Server running...");
+        try (ServerSocket serverSocket = new ServerSocket(8189)) {
+            while (true) {
+                try {
+                    new TCPConnection(serverSocket.accept(), this);
+                } catch (IOException e) {
+                    System.out.println("TCPConnection exception " + e);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public synchronized void onConnectionReady(TCPConnection tcpConnection) {
+        connections.add(tcpConnection);
+        sendToAllConnection("Client connected: " + tcpConnection);
+    }
+
+    @Override
+    public synchronized void onReceiveString(TCPConnection tcpConnection, String value) {
+        sendToAllConnection(value);
+    }
+
+    @Override
+    public synchronized void onDisconnect(TCPConnection tcpConnection) {
+        connections.remove(tcpConnection);
+        sendToAllConnection("Client disconnected: " + tcpConnection);
+    }
+
+    @Override
+    public synchronized void onException(TCPConnection tcpConnection, Exception e) {
+        System.out.println("TCPConnecion error " + e);
+    }
+
+    private void sendToAllConnection(String value){
+        System.out.println(value);
+        for (TCPConnection t: connections)
+            t.sendString(value);
+    }
+}
